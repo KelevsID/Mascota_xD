fa70c9d63d5026d02189421ce0e07b5796a6104c
KelevsID <johndoe@example.com>
2021-11-22 00:30:39 -0500

    Versión 1.1.2 de la aplicación de mascota

---
+++ Variables.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov 11 01:19:45 2021
+
+@author: posta
+"""
+
+import os
+
+os.environ['TWILIO_ACCOUNT_SID'] = "SID del twilio"   
+os.environ['TWILIO_AUTH_TOKEN'] = "Auth token twilio "   
+os.environ['SENDGRID_API_KEY'] = "sengrid api key"
+os.environ['Test'] = "Prueba de Variable de Entorno"
